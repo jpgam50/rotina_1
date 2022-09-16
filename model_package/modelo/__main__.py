4bcb3948073e016e24a02488afec5ec76a2cdc34
joaogambaro <joaopaulogambaro@gmail.com>
2022-09-16 00:03:16 -0300

    teste com .sh

--- model_package/modelo/__main__.py
+++ model_package/modelo/__main__.py
@@ -1,13 +1,10 @@
 from modelo.treina_modelo import treina_pipeline
 
-#--comentaraio para verificar a sobreposicao do packkote no gemfurry---
 
 def main():
     print("Rodar:")
     print("--> python -m modelo.treina_modelo, para treinar o modelo")
     print("--> python -m modelo.preve_modelo, para prever com o modelo")
-    print("comentaraio para verificar a sobreposicao do packkote no gemfurry")
-
 
 
 #entry point
